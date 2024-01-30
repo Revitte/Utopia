acb9b626edbc5a0765c338a2db4525a697f6af47
Revitte <51162772+Revitte@users.noreply.github.com>
2024-01-30 05:45:27 -0300

    Injeção de dependências no Spring
    Injeção de dependências no Spring
    
    Com esse commit entendemos como faremos as injeções de dependências, sendo a melhor maneira (no momento atual 2024-01-30) usar um contrutor na Controller.
    E a forma como a gente envia os dados é ultilizando um Model and View.
    
    Obs: Cod exemplo usado para adicionar dados no banco local h2:
    
    INSERT INTO TODO(TITLE, DEAD_LINE, CREATED_AT) VALUES('Estudar Spring', '2024-05-08', '2024-01-30 05:00:00.0');

--- src/main/java/br/com/oitavo/utopia/controllers/HomeController.java
+++ src/main/java/br/com/oitavo/utopia/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package br.com.oitavo.utopia.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.oitavo.utopia.repositories.Todorepository;
 
 
 
@@ -9,9 +14,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private final Todorepository todoRepository;
+
+    public HomeController(Todorepository todorepository){
+        this.todoRepository = todorepository;
+    }
+
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public ModelAndView home(){
+        var modelAndView = new ModelAndView("home");
+        modelAndView.addObject("nome", "Otávio Revitte");
+
+        var alunos = List.of("Cleyson Lima", "Wesley Gado", "Elen Fonseca", "Beatriz Ana");
+        modelAndView.addObject("alunos", alunos);
+
+        modelAndView.addObject("ehProgramador", false);
+
+        var todos = todoRepository.findAll();
+        System.out.println(todos);
+
+        return modelAndView;
     }
     
 }
